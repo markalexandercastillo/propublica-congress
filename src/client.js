2f6e4ace014dc6bcdd6b3a59636bdd25865faa1c
Mark Castillo <markalexandercastillo@gmail.com>
2017-02-08 21:58:37 -0500

    Some documentation

--- src/client.js
+++ src/client.js
@@ -1,3 +1,8 @@
+/**
+ * Low-level client to the ProPublica congress API. Encapsulates behavior
+ * common to all API interactions.
+ */
+
 const http = require('./http')
   , {create, assign} = Object;
 
@@ -7,20 +12,33 @@ const defaults = {
 };
 
 const proto = {
+  /**
+   * HTTP get to the API
+   * @param  {String} endpoint   API endpoint fragment eg. 'members/new'
+   * @param  {Number} [offset=0]
+   * @return {Promise}
+   */
   get(endpoint, offset = 0) {
     const headers = {['X-API-Key']: this.key};
     const body = offset ? {offset} : {};
     return http.get(
       `${this.host}/congress/v${this.version}/${endpoint}.json`,
       assign({headers, json: true}, {body})
-      // pluck out relevant data from the body probably from having to
-      // support xml
+    // pluck out relevant data from the body probably from having to
+    // support xml
     ).then(response => response.body.results[0]);
   }
 };
 
 module.exports = {
   defaults,
+  /**
+   * Factory function for getting client instances
+   * @param  {options.String} [version='1']
+   * @param  {options.String} [host='https://api.propublica.org']
+   * @param  {options.String} key                                 API Key
+   * @return {Object}
+   */
   create({
     version = defaults.VERSION,
     host = defaults.HOST,
