d5b6ffb16f892099b09655219280084805d43e66
Mark Castillo <markalexandercastillo@gmail.com>
2017-02-16 16:38:48 -0500

    Mocking out the validations dependency for non-sociable tests

--- test/client.js
+++ test/client.js
@@ -10,10 +10,16 @@ require('chai').use(require('chai-as-promised')).should();
 const ignoreExtraArgs = true;
 
 describe('client', () => {
-  let http, createClient;
+  let http, createClient, validators;
   beforeEach(() => {
     http = replace('./../src/http');
+    validators = replace('./../src/validators');
     createClient = require('./../src/client').create;
+
+    // validation invocations pass by default
+    when(validators.isValidOffset(anything())).thenReturn(true);
+    when(validators.isValidResponse(anything())).thenReturn(true);
+    when(validators.isValidApiKey(anything())).thenReturn(true);
   });
 
   describe('create()', () => {
@@ -22,10 +28,10 @@ describe('client', () => {
       () => createClient('SOME_KEY').key.should.equal('SOME_KEY')
     );
 
-    it(
-      'throws with an invalid key',
-      () => (() => createClient()).should.throw(Error, 'Received invalid API key:')
-    );
+    it('throws with an invalid key', () => {
+      when(validators.isValidApiKey(anything())).thenReturn(false);
+      (() => createClient()).should.throw(Error, 'Received invalid API key:');
+    });
   });
 
   describe('.get()', () => {
@@ -90,11 +96,13 @@ describe('client', () => {
     });
 
     it("rejects if the response came back with an invalid response structure", () => {
+      when(validators.isValidResponse('invalid response structure')).thenReturn(false);
       when(http.get(), {ignoreExtraArgs}).thenResolve('invalid response structure');
       client.get('some/endpoint').should.be.rejectedWith(Error, 'Received invalid response structure:');
     });
 
     it('rejects if given an invalid offset value', () => {
+      when(validators.isValidOffset('an invalid offset')).thenReturn(false);
       client.get('some/endpoint', 'an invalid offset').should.be.rejectedWith(Error, 'Received invalid offset:');
     });
   });
