daa5a753a876650fc8eb8aaef5e366839cae2080
Mark Castillo <markalexandercastillo@gmail.com>
2017-02-16 16:33:24 -0500

    Trimming down code
    * Deeply plucking out functions coming from testdouble
    *  No longer setting chai

--- test/client.js
+++ test/client.js
@@ -1,13 +1,18 @@
-const td = require('testdouble')
-  , URL = require('url')
-  , chai = require('chai');
+const {replace, when, verify, matchers: {
+  anything,
+  contains,
+  argThat
+}} = require('testdouble')
+  , URL = require('url');
 
-chai.use(require('chai-as-promised')).should();
+require('chai').use(require('chai-as-promised')).should();
+
+const ignoreExtraArgs = true;
 
 describe('client', () => {
   let http, createClient;
   beforeEach(() => {
-    http = td.replace('./../src/http');
+    http = replace('./../src/http');
     createClient = require('./../src/client').create;
   });
 
@@ -27,72 +32,65 @@ describe('client', () => {
     let client;
     beforeEach(() => {
       client = createClient('SOME_KEY');
-
-      td.when(http.get(), {ignoreExtraArgs: true})
-        .thenResolve({body: {results: []}});
+      when(http.get(), {ignoreExtraArgs}).thenResolve({body: {results: ['']}});
     });
 
     it("sets the 'X-API-Key' header with the given key", () => {
       client.get('some/endpoint');
-      td.verify(http.get(td.matchers.anything(), td.matchers.contains({
-        headers: {'X-API-Key': 'SOME_KEY'}
-      })));
+      verify(http.get(
+        anything(),
+        contains({headers: {'X-API-Key': 'SOME_KEY'}})
+      ));
     });
 
     it("expects a JSON response", () => {
       client.get('some/endpoint');
-      td.verify(http.get(td.matchers.anything(), td.matchers.contains({
-        json: true
-      })));
+      verify(http.get(
+        anything(),
+        contains({json: true})
+      ));
     });
 
     it("sets an offset from the second argument", () => {
       client.get('some/endpoint', 20);
-      td.verify(http.get(td.matchers.anything(), td.matchers.contains({
-        body: {offset: 20}
-      })));
+      verify(http.get(
+        anything(),
+        contains({body: {offset: 20}})
+      ));
     });
 
     it("performs the request to the ProPublica API host", () => {
       client.get('some/endpoint');
-      td.verify(http.get(
-        td.matchers.argThat(url => url.indexOf('https://api.propublica.org') === 0),
-        td.matchers.anything()
+      verify(http.get(
+        argThat(url => url.indexOf('https://api.propublica.org') === 0),
+        anything()
       ));
     });
 
 
     it("performs the request to version 1 of ProPublica's Congres API", () => {
       client.get('some/endpoint');
-      td.verify(http.get(
-        td.matchers.argThat(url => {
-          return URL.parse(url).path.indexOf('/congress/v1/') === 0;
-        }),
-        td.matchers.anything()
+      verify(http.get(
+        argThat(url => URL.parse(url).path.indexOf('/congress/v1/') === 0),
+        anything()
       ));
     });
 
     it("performs the request to the JSON variant of the endpoint", () => {
       client.get('some/endpoint');
-      td.verify(http.get(
-        td.matchers.argThat(url => {
-          return url.substr(-'some/endpoint.json'.length) === 'some/endpoint.json';
-        }),
-        td.matchers.anything()
+      verify(http.get(
+        argThat(url => url.substr(-'some/endpoint.json'.length) === 'some/endpoint.json'),
+        anything()
       ));
     });
 
     it("resolves to the first element of the 'results' key of the response body", () => {
-      td.when(http.get(), {ignoreExtraArgs: true})
-        .thenResolve({body: {results: ['relevantData']}});
-
+      when(http.get(), {ignoreExtraArgs}).thenResolve({body: {results: ['relevantData']}});
       client.get('some/endpint').should.become('relevantData');
     });
 
     it("rejects if the response came back with an invalid response structure", () => {
-      td.when(http.get(), {ignoreExtraArgs: true})
-        .thenResolve('invalid response structure');
-
+      when(http.get(), {ignoreExtraArgs}).thenResolve('invalid response structure');
       client.get('some/endpoint').should.be.rejectedWith(Error, 'Received invalid response structure:');
     });
 
