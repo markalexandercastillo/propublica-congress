0021f4185ba057fe7429b1e291b94a677ebf6ac2
Mark Castillo <markalexandercastillo@gmail.com>
2017-02-16 10:59:42 -0500

    Fixing doc

--- src/validators.js
+++ src/validators.js
@@ -1,6 +1,7 @@
 /**
  * Whether the argument is a valid offset to send to the ProPublica API
- * @param {Number} offset
+ * @param {String|Number} offset
+ * @return {Boolean}
  */
 function isValidOffset(offset) {
   return (offset % 20) === 0 && offset !== '';
