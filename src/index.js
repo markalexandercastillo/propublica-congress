8980353fb2736c4184db5f61a8dd101d6de93e43
Mark Castillo <markalexandercastillo@gmail.com>
2017-02-18 16:39:22 -0500

    Validating congress on ppc create

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const {create, assign} = Object
+  , {stringify} = JSON
   , client = require('./client')
+  , validators = require('./validators')
   ;
 
 const proto = {
@@ -7,6 +9,9 @@ const proto = {
 
 module.exports = {
   create(key, congress) {
+    if (congress && !validators.isValidCongress(congress)) {
+      throw new Error(`Received invalid congress: ${stringify(congress)}`);
+    }
     return assign(create(proto), {
       congress,
       client: client.create(key)
--- test/index.js
+++ test/index.js
@@ -1,16 +1,23 @@
-const {replace, when} = require('testdouble');
+const {replace, when, matchers: {anything}} = require('testdouble');
 
 require('chai').use(require('chai-as-promised')).should();
 
 describe('pro-publica-congress', () => {
-  let client, createPpc;
+  let client, validators, createPpc;
   beforeEach(() => {
     client = replace('./../src/client');
+    validators = replace('./../src/validators');
     createPpc = require('./../src/index').create;
+    when(validators.isValidCongress(anything())).thenReturn(true);
   });
 
   describe('create()', () => {
     it("sets a 'congress' property to the given argument", () => createPpc('SOME_KEY', 115).congress.should.equal(115));
+
+    it('throws with an invalid congress', () => {
+      when(validators.isValidCongress(116)).thenReturn(false);
+      (() => createPpc('SOME_KEY', 116)).should.throw(Error, 'Received invalid congress:');
+    });
 
     it("sets a 'client' property to a client created with the given key argument", () => {
       const expectedClient = {};
