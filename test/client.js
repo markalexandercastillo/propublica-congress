4a52ff4301caef7c44ffd74a0369f61b5ac3ad72
Mark Castillo <markalexandercastillo@gmail.com>
2017-02-18 22:38:33 -0500

    Ensuring that promise-based tests get errors reported correctly here too. And using ignoreExtraArgs where possible. And breaking up lines.

--- test/client.js
+++ test/client.js
@@ -17,9 +17,9 @@ describe('client', () => {
     createClient = require('./../src/client').create;
 
     // validation invocations pass by default
-    when(validators.isValidOffset(anything())).thenReturn(true);
-    when(validators.isValidResponse(anything())).thenReturn(true);
-    when(validators.isValidApiKey(anything())).thenReturn(true);
+    when(validators.isValidOffset(), {ignoreExtraArgs}).thenReturn(true);
+    when(validators.isValidResponse(), {ignoreExtraArgs}).thenReturn(true);
+    when(validators.isValidApiKey(), {ignoreExtraArgs}).thenReturn(true);
   });
 
   describe('create()', () => {
@@ -42,68 +42,67 @@ describe('client', () => {
     });
 
     it("sets the 'X-API-Key' header with the given key", () => {
-      client.get('some/endpoint');
-      verify(http.get(
-        anything(),
-        contains({headers: {'X-API-Key': 'SOME_KEY'}})
-      ));
+      return client.get('some/endpoint')
+        .then(() => verify(http.get(
+          anything(),
+          contains({headers: {'X-API-Key': 'SOME_KEY'}})
+        )));
     });
 
     it("expects a JSON response", () => {
-      client.get('some/endpoint');
-      verify(http.get(
-        anything(),
-        contains({json: true})
-      ));
+      return client.get('some/endpoint')
+        .then(() => verify(http.get(
+          anything(),
+          contains({json: true})
+        )));
     });
 
     it("sets an offset from the second argument", () => {
-      client.get('some/endpoint', 20);
-      verify(http.get(
-        anything(),
-        contains({body: {offset: 20}})
-      ));
+      return client.get('some/endpoint', 20)
+        .then(() => verify(http.get(
+          anything(),
+          contains({body: {offset: 20}})
+        )));
     });
 
     it("performs the request to the ProPublica API host", () => {
-      client.get('some/endpoint');
-      verify(http.get(
-        argThat(url => url.indexOf('https://api.propublica.org') === 0),
-        anything()
-      ));
+      return client.get('some/endpoint')
+        .then(() => verify(http.get(
+          argThat(url => url.indexOf('https://api.propublica.org') === 0)
+        ), {ignoreExtraArgs}));
     });
 
 
     it("performs the request to version 1 of ProPublica's Congres API", () => {
-      client.get('some/endpoint');
-      verify(http.get(
-        argThat(url => URL.parse(url).path.indexOf('/congress/v1/') === 0),
-        anything()
-      ));
+      return client.get('some/endpoint')
+        .then(() => verify(http.get(
+          argThat(url => URL.parse(url).path.indexOf('/congress/v1/') === 0)
+        ), {ignoreExtraArgs}));
     });
 
     it("performs the request to the JSON variant of the endpoint", () => {
-      client.get('some/endpoint');
-      verify(http.get(
-        argThat(url => url.substr(-'some/endpoint.json'.length) === 'some/endpoint.json'),
-        anything()
-      ));
+      return client.get('some/endpoint')
+        .then(() => verify(http.get(
+          argThat(url => url.substr(-'some/endpoint.json'.length) === 'some/endpoint.json')
+        ), {ignoreExtraArgs}));
     });
 
     it("resolves to the first element of the 'results' key of the response body", () => {
       when(http.get(), {ignoreExtraArgs}).thenResolve({body: {results: ['relevantData']}});
-      client.get('some/endpint').should.become('relevantData');
+      return client.get('some/endpint').should.become('relevantData');
     });
 
     it("rejects if the response came back with an invalid response structure", () => {
       when(validators.isValidResponse('invalid response structure')).thenReturn(false);
       when(http.get(), {ignoreExtraArgs}).thenResolve('invalid response structure');
-      client.get('some/endpoint').should.be.rejectedWith(Error, 'Received invalid response structure:');
+      return client.get('some/endpoint')
+        .should.be.rejectedWith(Error, 'Received invalid response structure:');
     });
 
     it('rejects if given an invalid offset value', () => {
       when(validators.isValidOffset('an invalid offset')).thenReturn(false);
-      client.get('some/endpoint', 'an invalid offset').should.be.rejectedWith(Error, 'Received invalid offset:');
+      return client.get('some/endpoint', 'an invalid offset')
+        .should.be.rejectedWith(Error, 'Received invalid offset:');
     });
   });
 });
